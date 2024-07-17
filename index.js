9a40357beff63e99ef33f6318a01643e034533a4
Praveen kumar <zwolfconsultancyserives@gmail.com>
2024-07-17 10:53:25 +0530

    push

--- index.js
+++ index.js
@@ -1,30 +1,31 @@
 const express = require("express");
-const mongoose=require("mongoose");
-const bodyParser=require('body-parser')
-const userRoute=require("./Routes/UserRoutes.js")
-const adminroutes=require("./Routes/adminroutes.js")
-const cors=require('cors')
-const morgan=require("morgan")
+const mongoose = require("mongoose");
+const bodyParser = require('body-parser')
+const userRoute = require("./Routes/UserRoutes.js")
+const adminroutes = require("./Routes/adminroutes.js")
+const cors = require('cors')
+const morgan = require("morgan")
 require("dotenv").config()
 
+const port = process.env.PORT || 6000
 const app = express()
 app.use(cors())
 
-app.use(bodyParser.json({extended:true,limit:"5mb"}))
-app.use(bodyParser.urlencoded({extended:true,limit:"5mb"}))
-app.use(express.json({extended:true,limit:"5mb"}))
-app.use(express.urlencoded({extended:true,limit:"5mb"}))
+app.use(bodyParser.json({ extended: true, limit: "5mb" }))
+app.use(bodyParser.urlencoded({ extended: true, limit: "5mb" }))
+app.use(express.json({ extended: true, limit: "5mb" }))
+app.use(express.urlencoded({ extended: true, limit: "5mb" }))
 
 app.use(morgan("dev"))
-app.use("/user",userRoute)  
-app.use("/admin",adminroutes)
-app.use("/ ", (req, res) => {
+app.use("/user", userRoute)
+app.use("/admin", adminroutes)
+app.use("/", (req, res) => {
     res.send('server is running  ')
 });
 
-mongoose.connect(process.env.mongoDB_URL,{})
+mongoose.connect(process.env.mongoDB_URL, {})
     .then(() => {
-        app.listen(process.env.Port, () => {
+        app.listen(port, () => {
             console.log("server is up and running");
             console.log("connected to the database");
         })
@@ -33,5 +34,3 @@ mongoose.connect(process.env.mongoDB_URL,{})
     .catch((err) => {
         console.log(err.message);
     })
-
-
